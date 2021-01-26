d3887d3694aa84cb8d0b33efa344ba6939b60c4a
CristinaOanaManole <oana_manole@yahoo.com>
2021-01-26 11:46:15 +0100

    maj

--- FirstBDD.sql
+++ FirstBDD.sql
@@ -310,14 +310,13 @@ INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Django',11);
 INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Ruby On Rails',12);
 
 /*exo1-Dans la table frameworks, afficher toutes les données de la table ayant une version 2.x (x étant un numéro quelconque).*/
-SELECT * FROM `frameworks` WHERE 'new'
-
+SELECT * FROM `frameworks` WHERE `version` LIKE '2.%';
 
 /*exo2-Dans la table frameworks, afficher toutes les lignes ayant pour id 1 et 3.*/
-
+SELECT * FROM `frameworks` WHERE `id`IN (1, 3);
 
 /*exo3-Dans la table ide, afficher toutes les lignes ayant une date comprise entre le premier janvier 2010 et le 31 decembre 2011.*/
-
+SELECT * FROM `ide` WHERE `date` BETWEEN 2010-01-01 AND 2011-12-31;
 
 /* PARTIE 7 - Suppression et modification de données */
 
